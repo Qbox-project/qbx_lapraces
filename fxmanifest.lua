0b5170428892b1f6b63949acd96f8196527eaf37
Tony <97451137+TonybynMp4@users.noreply.github.com>
2022-11-11 17:53:35 +0100

    Fix last lap records, add locales, and update `.github` (#2)
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Apply suggestions from code review
    
    Co-authored-by: David Malchin <malchin459@gmail.com>
    
    * Update auto_assign.yml
    
    * Update fxmanifest.lua
    
    Co-authored-by: David Malchin <malchin459@gmail.com>
    
    Co-authored-by: David Malchin <malchin459@gmail.com>

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,12 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_script 'config.lua'
+shared_scripts {
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
+}
 
 client_script 'client/main.lua'
 server_scripts {
