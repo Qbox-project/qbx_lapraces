5825f4967c53117bf5c0d95e01c8545b68037e25
ChatDisabled <44729807+ChatDisabled@users.noreply.github.com>
2023-04-04 22:57:42 +0200

    fix: export naming

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,4 @@
-local QBCore = exports['qb-core']:GetCoreObject()
+local QBCore = exports['qbx-core']:GetCoreObject()
 local Countdown = 10
 local ToFarCountdown = 10
 local FinishedUITimeout = false
