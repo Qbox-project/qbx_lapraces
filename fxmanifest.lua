989b36f2fe6ec9e9b12e89bf07c4fc62e4b50632
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2023-06-10 22:20:38 +0200

    feat(manifest): oal

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QB-LapRaces'
+description 'QBX-Lapraces'
 version '1.0.0'
 
 ui_page 'html/index.html'
 
 shared_scripts {
+    '@ox_lib/init.lua',
     '@qb-core/shared/locale.lua',
     'locales/en.lua',
     'locales/*.lua',
@@ -14,6 +15,7 @@ shared_scripts {
 }
 
 client_script 'client/main.lua'
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
@@ -26,4 +28,6 @@ files {
     'html/img/*'
 }
 
+provide 'qb-lapraces'
 lua54 'yes'
+use_experimental_fxv2_oal 'yes'
